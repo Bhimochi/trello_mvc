d0a383781d096f17bea2428abede69a53fdb4053
Bhi Wong <mochi@DESKTOP-JB6D21O.localdomain>
2022-11-09 01:24:11 +1100

    Complete

--- controllers/cards_controller.py
+++ controllers/cards_controller.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request
 from init import db
 from datetime import date
 from models.card import Card, CardSchema
-
+from controllers.auth_controller import authorize
+from flask_jwt_extended import jwt_required
 cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
 
 @cards_bp.route('/')
@@ -29,7 +30,10 @@ def get_one_card(id):
         return {'error': f'Card not found with id {id}'}, 404
 
 @cards_bp.route('/<int:id>/', methods=['DELETE'])
+@jwt_required()
 def delete_one_card(id):
+    authorize()
+
     stmt = db.select(Card).filter_by(id=id)
     card = db.session.scalar(stmt)
     if card:
@@ -42,6 +46,7 @@ def delete_one_card(id):
 
 
 @cards_bp.route('/<int:id>/',methods=['PUT', 'PATCH'])
+@jwt_required()
 def update_one_card(id):
     stmt = db.select(Card).filter_by(id=id)
     card = db.session.scalar(stmt)
@@ -57,6 +62,7 @@ def update_one_card(id):
 
 
 @cards_bp.route('/', methods=['POST'])
+@jwt_required()
 def create_card():
         # Create a new Card model instance
         card = Card(
@@ -67,7 +73,7 @@ def create_card():
             priority = request.json['priority']
         )
         # Add and commit user to DB
-        db.session.add(user)
+        db.session.add(card)
         db.session.commit()
         # Respond to client
         return CardSchema().dump(card), 201
